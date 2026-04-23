e2e39051580cc20a2ce0d23f104f857d5f6aa6fa
kzynzero <qlecaros@gmail.com>
2026-04-23 21:40:10 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daily Planner v2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css              " rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="splash-wrapper">
+    <div class="logo">
+        <img src="image/logo.png" alt="logo" style="width: 50%; height: auto;">
+    </div>
+
+    <a href="pages/todo.php" class="btn-start">start</a>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
